f09d404002b606f5df460eb1a798fd5c3c5554b3
Esteban Lorenzano <estebanlm@gmail.com>
2016-01-25 13:48:50 +0100

    add travis (first attempt)

---
+++ scripts/build-tokyocabinet.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+$default_tokyo_version="1.4.48"
+
+if [ $1 == "" ]; then
+	tokyo_version="$1"
+else
+	tokyo_version="$default_tokyo_version"
+fi	
+
+wget http://fallabs.com/tokyocabinet/tokyocabinet-$tokyo_version.tar.gz
+tar xfz tokyocabinet-$tokyo_version.tar.gz
+cd tokyocabinet-$tokyo_version
+./configure
+make
+# put libraries in the root build
+cp -f lib* ../..
