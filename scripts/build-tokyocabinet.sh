133bbf9789b35ead544526612810b80d9bfeafe7
Esteban Lorenzano <estebanlm@gmail.com>
2016-01-25 16:27:08 +0100

    retry bzip2

--- scripts/build-tokyocabinet.sh
+++ scripts/build-tokyocabinet.sh
@@ -13,7 +13,7 @@ fi
 wget http://fallabs.com/tokyocabinet/tokyocabinet-$tokyo_version.tar.gz
 tar xfz tokyocabinet-$tokyo_version.tar.gz
 cd tokyocabinet-$tokyo_version
-CFLAGS="-m32" LDFLAGS="-m32" ./configure --disable-bzip
+CFLAGS="-m32" LDFLAGS="-m32" ./configure
 make
 # put libraries in the root build 
 echo -n "Copying library to root path"
